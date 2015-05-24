b352c03c39b147f59a334d2b2e867df64a5048bf
georgecode <georgeczurlanis@gmail.com>
2015-05-24 06:38:08 -0400

    some work on nav bar some bug fixes to main content

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ $(".red").mouseenter(function() {
 	if ($(window).width() > 767) {
     $(".aboutMe").animate(
             {"height": "100%"},
-            "slow").css({'background-color': 'rgba(249,199,0,0.95)', 'transition': 'background-color 1.0s ease'});
+            "slow").css({
+                'background-color': 'rgba(249,199,0,0.95)', 
+                'transition': 'background-color 1.0s ease'
+                });
     /*$(".hideMe").css({'display':'inline'});*/
     $(".hideMe").fadeIn(500)
  	}
