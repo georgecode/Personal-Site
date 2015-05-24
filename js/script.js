f753df20fe15c730c243cfa4d7893b5d674b0c20
georgecode <georgeczurlanis@gmail.com>
2015-05-24 04:34:44 -0400

    added content boxes boot strap and started testing ideas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$('.test').on('click', function(){
-	$('.hello').toggle()
-})
+
+
+ 
+
+$(".red").mouseenter(function() {
+	if ($(window).width() > 767) {
+    $(".aboutMe").animate(
+            {"height": "100%"},
+            "slow").css({'background-color': 'rgba(249,199,0,0.95)', 'transition': 'background-color 1.0s ease'});
+    /*$(".hideMe").css({'display':'inline'});*/
+    $(".hideMe").fadeIn(500)
+ 	}
+});
+
+$(".red").mouseleave(function() {
+	if ($(window).width() > 767) {
+    $(".aboutMe").animate(
+            {"height": "100px"},
+            "slow").css({'background-color': 'rgba(249,199,0,0.6)'});
+    /*$(".hideMe").css({'display':'none'});*/
+    $(".hideMe").fadeOut(500)
+	}
+});
+
