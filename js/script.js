8e861b4017f9351151ffe4b245f294089d7e33ba
georgecode <georgeczurlanis@gmail.com>
2015-06-03 21:48:53 -0400

    fixed bug in animationfixed scree width error

--- js/script.js
+++ js/script.js
@@ -1,27 +1,68 @@
 
+//782px stops working
+//767px is moble
 
- 
+var animateTest = true;
 
-$(".red").mouseenter(function() {
-	if ($(window).width() > 767) {
-    $(".aboutMe").animate(
+
+    $(".red").mouseenter(function() {
+        
+
+    	if (window.matchMedia('(min-width: 767px)').matches) {
+            
+           
+            $(".aboutMe").stop(true,false).animate(
+                    {"height": "100%"},
+                    "slow").css({
+                        'background-color': 'rgba(249,199,0,0.95)', 
+                        'transition': 'background-color 1.0s ease'
+                        });
+            /*$(".hideMe").css({'display':'inline'});*/
+            $(".hideMe").stop(true,false).fadeIn(500)
+     	}
+    });
+
+
+
+
+
+
+    $(".red").mouseleave(function() {
+
+    	if (window.matchMedia('(min-width: 767px)').matches) {
+        $(".aboutMe").stop(true,false).animate(
+                {"height": "100px"},
+                "slow").css({'background-color': 'rgba(249,199,0,0.6)'});
+        /*$(".hideMe").css({'display':'none'});*/
+        $(".hideMe").stop(true,false).fadeOut(500)
+        
+    	}
+    });
+
+
+
+//---------------------------------------
+
+$(".green").mouseenter(function() {
+    if ($(window).width() > 767) {
+    $(".gggaboutMe").animate(
             {"height": "100%"},
             "slow").css({
                 'background-color': 'rgba(249,199,0,0.95)', 
                 'transition': 'background-color 1.0s ease'
                 });
     /*$(".hideMe").css({'display':'inline'});*/
-    $(".hideMe").fadeIn(500)
- 	}
+    $(".ggghideMe").fadeIn(500)
+    }
 });
 
-$(".red").mouseleave(function() {
-	if ($(window).width() > 767) {
-    $(".aboutMe").animate(
+$(".green").mouseleave(function() {
+    if ($(window).width() > 767) {
+    $(".gggaboutMe").animate(
             {"height": "100px"},
             "slow").css({'background-color': 'rgba(249,199,0,0.6)'});
     /*$(".hideMe").css({'display':'none'});*/
-    $(".hideMe").fadeOut(500)
-	}
+    $(".ggghideMe").fadeOut(500)
+    }
 });
 
